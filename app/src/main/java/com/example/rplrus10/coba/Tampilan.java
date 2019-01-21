02577c4c61ffd40ab48f99db947764ef35577b3b
jelitaSalsabila <jelitasalsa9a@gmail.com>
2019-01-21 12:18:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rplrus10/coba/Tampilan.java
@@ -0,0 +1,13 @@
+package com.example.rplrus10.coba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tampilan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan);
+    }
+}
