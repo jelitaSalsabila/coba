8600115c1ae5853846d8da266d624b93c096696e
Muhammadidrisjrnx <idris.xrplsmkrus@gmail.com>
2019-01-21 12:37:57 +0700

    ..

--- app/src/main/java/com/example/rplrus10/coba/Tampilan.java
+++ app/src/main/java/com/example/rplrus10/coba/Tampilan.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Tampilan extends AppCompatActivity {
-
+    // menampilkan recycleview dan asnyctask
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/rplrus10/coba/adapter_recycle_view.java
@@ -0,0 +1,6 @@
+package com.example.rplrus10.coba;
+
+public class adapter_recycle_view {
+    // tempat menaruh dan mensetting recycle view
+    // layout nya namanya row_item.xml
+}
